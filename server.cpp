b1442daa5813869076b2c1f9c67040cd16312c10
William Yue <williamyue37@gmail.com>
2019-04-09 15:59:58 -0500

    server update

--- server.cpp
+++ server.cpp
@@ -2,12 +2,18 @@
 #include <winsock2.h>
  
 using namespace std;
+
+const int MAX_SZ = 1e4;
  
+struct Player {
+    int r, c, s; //r and c is the player's current position, while s is 1 if the player is shooting and 0 otherwise.
+} p1, p2;
+
 int main()
 {
     WSADATA WSAData;
  
-    SOCKET server, client;
+    SOCKET server, client1, client2;
  
     SOCKADDR_IN serverAddr, clientAddr;
  
@@ -23,16 +29,37 @@ int main()
  
     cout << "Listening for incoming connections..." << endl;
  
-    char buffer[1024];
+    char* buffer;
+    string message;
     int clientAddrSize = sizeof(clientAddr);
-    if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
-    {
+    if ((client1 = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
         cout << "Client connected!" << endl;
-        recv(client, buffer, sizeof(buffer), 0);
-        cout << "Client says: " << buffer << endl;
-        memset(buffer, 0, sizeof(buffer));
- 
-        closesocket(client);
-        cout << "Client disconnected." << endl;
+        buffer = "1";
+        send(client1, buffer, sizeof(buffer), 0);
+        p1 = {0, 0, 0};
+    }
+    if ((client2 = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
+        cout << "Client connected!" << endl;
+        buffer = "2";
+        send(client2, buffer, sizeof(buffer), 0);
+        p2 = {10, 5, 0};
+    }
+
+    while (true) {
+        //server updates clients with the positions of both players in the form "r1 c1 s1|r2 c2 s2"
+        message = to_string(p1.r) + " " + to_string(p1.c) + " " + to_string(p1.s) + "|" + to_string(p2.r) + " " + to_string(p2.c) + " " + to_string(p2.s);
+        send(client1, buffer, sizeof(buffer), 0);
+        send(client2, buffer, sizeof(buffer), 0);
+
+        //client updates server with their players current state in the form of "r c s"
+        recv(client1, buffer, sizeof(buffer), 0);
+        p1.r = stoi(strtok(buffer, " "));
+        p1.c = stoi(strtok(NULL, " "));
+        p1.s = stoi(strtok(NULL, " "));
+
+        recv(client2, buffer, sizeof(buffer), 0);
+        p2.r = stoi(strtok(buffer, " "));
+        p2.c = stoi(strtok(NULL, " "));
+        p2.s = stoi(strtok(NULL, " "));
     }
 }
